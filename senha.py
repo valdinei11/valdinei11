bcc3720216856a1deed14be276e5568f45644ffb
valdinei11 <131720266+valdinei11@users.noreply.github.com>
2023-09-14 20:21:30 +0200

    Create senha.py

---
+++ senha.py
@@ -0,0 +1,9 @@
+print('BANK DA ALEMANHA')
+
+pin = int(input('Digite sua senha : '))
+
+while pin != 1234:
+  pin = int(input('Senha incorreta. Digite a senha novamente:'))
+if pin ==1234:
+  print('Senha aceita !')
+
